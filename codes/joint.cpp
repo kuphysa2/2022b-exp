e170e02d309e32d6311273d0c890100c132a22a6
kuphysa2 <125865887+kuphysa2@users.noreply.github.com>
2023-03-17 13:49:30 +0900

    made cutted data(<2845)

---
+++ codes/joint.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    int ana_date = 317;
+    char ofs_name[64];
+    snprintf(ofs_name, 64, "../exp0000/a%04d/exp0000.dat", ana_date);
+
+    ofstream ofs(ofs_name);
+    // ifstream ifs1("../exp0216/a0313/exp0216_acalib.dat");
+    ifstream ifs2("../exp0227/a0310/exp0227.dat");
+    ifstream ifs3("../exp0310/a0317/exp0310.dat");
+
+    double adc1, adc2, tdc;
+
+    // while (!ifs1.eof())
+    // {
+    //     ifs1 >> adc1 >> adc2 >> tdc;
+    //     ofs << adc1 << " " << adc2 << " " << tdc << endl;
+    // }
+
+    while (!ifs2.eof())
+    {
+        ifs2 >> adc1 >> adc2 >> tdc;
+        // if (tdc < 3799.805)
+        {
+            ofs << adc1 << " " << adc2 << " " << tdc << endl;
+        }
+    }
+    while (!ifs3.eof())
+    {
+        ifs3 >> adc1 >> adc2 >> tdc;
+        // if (tdc > 3799.805)
+        {
+            ofs << adc1 << " " << adc2 << " " << tdc << endl;
+        }
+    }
+
+    return 0;
+}
--- codes/joint_cut.cpp
+++ codes/joint_cut.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int ana_date = 317;
     char ofs_name[64];
-    snprintf(ofs_name, 64, "../exp0000/a%04d/cut/exp0000_cut(<50).dat", ana_date);
+    snprintf(ofs_name, 64, "../exp0000/a%04d/cut/exp0000_cut(<2845).dat", ana_date);
 
     ofstream ofs(ofs_name);
     // ifstream ifs1("../exp0216/a0313/exp0216_acalib.dat");
@@ -24,7 +24,7 @@ int main()
     while (!ifs2.eof())
     {
         ifs2 >> adc1 >> adc2 >> tdc;
-        if (tdc < 3799.805)
+        if (tdc < 2845.528)
         {
             ofs << adc1 << " " << adc2 << " " << tdc << endl;
         }
@@ -32,7 +32,7 @@ int main()
     while (!ifs3.eof())
     {
         ifs3 >> adc1 >> adc2 >> tdc;
-        if (tdc > 3799.805)
+        if (tdc < 2845.528)
         {
             ofs << adc1 << " " << adc2 << " " << tdc << endl;
         }
